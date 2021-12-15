33c6508d1d35bf271059e651235d18acb5bb833a
paulsalamone <psalamone@gmail.com>
2021-12-15 16:01:48 +0100

    adding stripes and new logo

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import logo from "./assets/logo.png";
+import logo from "./assets/logo-new.svg";
 
 const Header = () => {
   // const [searchTerm, setSearchTerm] = useState("");
