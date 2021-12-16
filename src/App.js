1244f2e03e5df1d48cb23678ad36166bbbe20dc9
paulsalamone <psalamone@gmail.com>
2021-12-16 22:02:20 +0100

    made headlines smaller

--- src/App.js
+++ src/App.js
@@ -12,25 +12,21 @@ function App() {
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(true);
 
-  
-
   const fetchData = async () => {
     await axios
-      .get(`https://hn.algolia.com/api/v1/search?query=${search}&page=${page}&hitsPerPage=18`)
+      .get(
+        `https://hn.algolia.com/api/v1/search?query=${search}&page=${page}&hitsPerPage=18`
+      )
       .then((response) => {
-          setData(response.data);
+        setData(response.data);
       })
       .catch((error) => alert(error));
-      setLoading(false);
-
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchData();
   }, [search]);
-
-
-
 
   return (
     <>
@@ -40,54 +36,61 @@ function App() {
         setSearch={setSearch}
         setQuery={setQuery}
       />
-      {loading 
-      ? <div className="container">
+      {loading ? (
+        <div className="container">
           <div className="row">
             <div className="col-4"></div>
-              <div className="col-4">
-                <div style={{height: '300px'}}></div>
-                <div style={{textAlign: 'center', height: '2000px'}}>
-                  <div className="spinner-border text-light" role="status">
-                    <span className="sr-only">Loading...</span>
-                  </div>
+            <div className="col-4">
+              <div style={{ height: "300px" }}></div>
+              <div style={{ textAlign: "center", height: "2000px" }}>
+                <div className="spinner-border text-light" role="status">
+                  <span className="sr-only">Loading...</span>
                 </div>
+              </div>
             </div>
           </div>
         </div>
-      : search === "" 
-      ? 
-      <div className="container">
-        <div className="row">
-          <div className="col-4"></div>
+      ) : search === "" ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-4"></div>
             <div className="col-4">
-              <div style={{height: '300px'}}></div>
-              <div style={{textAlign: 'center', height: '2000px'}}>
+              <div style={{ height: "300px" }}></div>
+              <div style={{ textAlign: "center", height: "2000px" }}>
                 <div className="text-light">
-                  <span className="">Please provide a search term ...</span>
+                  <span className="instructions">
+                    Please provide a search term
+                  </span>{" "}
+                  <span className="instructions" id="green">
+                    ...
+                  </span>
                 </div>
               </div>
+            </div>
           </div>
         </div>
-      </div>
-      : data.nbHits === 0
-      ? 
-      <div className="container">
-        <div className="row">
-          <div className="col-4"></div>
+      ) : data.nbHits === 0 ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-4"></div>
             <div className="col-4">
-              <div style={{height: '300px'}}></div>
-              <div style={{textAlign: 'center', height: '2000px'}}>
+              <div style={{ height: "300px" }}></div>
+              <div style={{ textAlign: "center", height: "2000px" }}>
                 <div className="text-light">
-                  <span className="">We tried our best,<br />
-                  but couldn't find anything!</span>
+                  <span className="">
+                    We tried our best,
+                    <br />
+                    but couldn't find anything!
+                  </span>
                 </div>
               </div>
+            </div>
           </div>
         </div>
-    </div>
-    : <ItemList data={data} search={search} />
-      }
-    
+      ) : (
+        <ItemList data={data} search={search} />
+      )}
+
       <Footer />
     </>
   );
