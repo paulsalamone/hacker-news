cbb14459c3e7c3e46c9927b2a59e2778868484ed
paulsalamone <psalamone@gmail.com>
2021-12-17 13:57:29 +0100

    fix css of pagination buttons

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,26 +1,33 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import Pagination from './Pagination'
+import Pagination from "./Pagination";
 
 const ItemList = ({ data, search, page, setPage }) => {
-const colour = () => {
-  let result = "#";
-  var hexValues = ["0", "f"];
+  const colour = () => {
+    let result = "#";
+    var hexValues = ["0", "f"];
 
-  for (let i = 0; i < 6; i++) {
-    result += hexValues[Math.floor(Math.random() * hexValues.length)];
-  }
-  return result;
-};
+    for (let i = 0; i < 6; i++) {
+      result += hexValues[Math.floor(Math.random() * hexValues.length)];
+    }
+    console.log(result);
+    // result === "#000000" ? (result = "#030001") : (result = "#000000");
+    if (result === "#000000") {
+      result = "lightgreen";
+    }
+    console.log(result);
+    return result;
+  };
   return (
     <>
       <main>
-        {search === ''
-        ? <div></div>
-        : <p className="results-message">
-          Search results for: <b id="search-text">"{search}"</b>
-        </p>
-        }
+        {search === "" ? (
+          <div></div>
+        ) : (
+          <p className="results-message">
+            Search results for: <b id="search-text">"{search}"</b>
+          </p>
+        )}
         <Pagination data={data} page={page} setPage={setPage} />
         <div className="items-list">
           {data.hits.map((item, index) => {
@@ -30,7 +37,12 @@ const colour = () => {
                 style={{ backgroundColor: colour() }}
               >
                 <div key={index} className="headline-item">
-                  <a href={item.url} className="headline-title" target="_blank" rel="noreferrer">
+                  <a
+                    href={item.url}
+                    className="headline-title"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     {item.title}
                   </a>
                   <p className="headline-author">Author: {item.author}</p>
